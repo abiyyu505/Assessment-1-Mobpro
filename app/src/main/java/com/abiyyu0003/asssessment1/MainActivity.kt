3873a69d192f658b13b396986a8757b33110c500
Muhammad Abiyyu Fawwaz Jelcha <abiyyumuhammad045@gmail.com>
2026-04-08 02:48:52 +0700

    Menambahkan fitur input tinggi badan

--- app/src/main/java/com/abiyyu0003/asssessment1/MainActivity.kt
+++ app/src/main/java/com/abiyyu0003/asssessment1/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,12 +16,21 @@ import com.abiyyu0003.asssessment1.ui.screen.MainScreen
 import com.abiyyu0003.asssessment1.ui.theme.Asssessment1Theme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Asssessment1Theme {
-                MainScreen()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Cek Berat Ideal") }
+                        )
+                    }
+                ) { innerPadding ->
+                    MainScreen(innerPadding)
+                }
             }
         }
     }
--- app/src/main/java/com/abiyyu0003/asssessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/abiyyu0003/asssessment1/ui/screen/MainScreen.kt
@@ -2,29 +2,43 @@ package com.abiyyu0003.asssessment1.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun MainScreen() {
-    ScreenContent()
+fun MainScreen(innerPadding: PaddingValues) {
+    ScreenContent( innerPadding )
 }
 
 @Composable
-fun ScreenContent() {
+fun ScreenContent( innerPadding: PaddingValues ) {
+    var tinggi by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
+            .padding(innerPadding)
             .padding(16.dp),
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
         Text("Halaman Utama")
+
+        TextField(
+            value = tinggi,
+            onValueChange = { tinggi = it },
+            label = { Text("Tinggi Badan (cm)") }
+        )
     }
 }
