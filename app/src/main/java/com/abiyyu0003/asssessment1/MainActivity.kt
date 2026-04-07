b806719360f92361ee8ff4bd86ad0cd8257ca7a8
Muhammad Abiyyu Fawwaz Jelcha <abiyyumuhammad045@gmail.com>
2026-04-08 01:45:43 +0700

    Initial Coding

---
+++ app/src/main/java/com/abiyyu0003/asssessment1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.abiyyu0003.asssessment1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.abiyyu0003.asssessment1.ui.theme.Asssessment1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Asssessment1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Asssessment1Theme {
+        Greeting("Android")
+    }
+}
