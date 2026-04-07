c7d9e3fcc32a0196d434791bc50db34c731e19c2
Muhammad Abiyyu Fawwaz Jelcha <abiyyumuhammad045@gmail.com>
2026-04-08 04:14:23 +0700

    Menambahkan tombol hitung dan hasil perhitungan

--- app/src/main/java/com/abiyyu0003/asssessment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/abiyyu0003/asssessment1/ui/screen/MainScreen.kt
@@ -6,10 +6,12 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -61,6 +63,10 @@ fun ScreenContent( innerPadding: PaddingValues ) {
                 onClick = { gender = "Wanita" }
             )
             Text("Wanita")
+        }
+
+        Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
+            Text("Hitung")
         }
     }
 }
